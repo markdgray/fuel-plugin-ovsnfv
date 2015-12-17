097a9b5f67363d5678e0053eacb3e6bb71f58aac
Mark D. Gray <mark.d.gray@intel.com>
2015-12-17 09:58:14 +0000

    clone.pp: Add ',' to end of line as puppet module does not install
    Signed-off-by: Mark D. Gray <mark.d.gray@intel.com>

--- deployment_scripts/puppet/modules/ovsdpdk/manifests/clone.pp
+++ deployment_scripts/puppet/modules/ovsdpdk/manifests/clone.pp
@@ -31,25 +31,25 @@ class ovsdpdk::clone(
 
   exec { "wget dpdk":
     command => "rm -rf dpdk.zip $ovs_dpdk_dir && wget http://10.20.0.2:8080/plugins/fuel-plugin-ovsnfv-0.0/repositories/ubuntu/dpdk.zip && unzip dpdk.zip && mv dpdk-2.1.0 $ovs_dpdk_dir",
-    path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+    path    => "/usr/bin:/usr/sbin:/bin:/sbin",
     require => Package['unzip'],
   }
 
   exec { "wget ovs":
     command => "rm -rf ovs.zip $ovs_dir && wget http://10.20.0.2:8080/plugins/fuel-plugin-ovsnfv-0.0/repositories/ubuntu/ovs.zip && unzip ovs.zip && mv ovs-master $ovs_dir",
-    path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+    path    => "/usr/bin:/usr/sbin:/bin:/sbin",
     require => Package['unzip'],
   }
 
   exec { "wget networking_ovs_dpdk":
     command => "rm -rf networking-ovs-dpdk.zip $networking_ovs_dpdk_dir && wget http://10.20.0.2:8080/plugins/fuel-plugin-ovsnfv-0.0/repositories/ubuntu/networking-ovs-dpdk.zip && unzip networking-ovs-dpdk.zip && mv networking-ovs-dpdk $networking_ovs_dpdk_dir",
-    path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+    path    => "/usr/bin:/usr/sbin:/bin:/sbin",
     require => Package['unzip'],
   }
 
   exec { "install pbr":
-    command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovsnfv-0.0/repositories/ubuntu/pbr-1.8.1-py2.py3-none-any.whl && pip install pbr-1.8.1-py2.py3-none-any.whl"
-    path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+    command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovsnfv-0.0/repositories/ubuntu/pbr-1.8.1-py2.py3-none-any.whl && pip install pbr-1.8.1-py2.py3-none-any.whl",
+    path    => "/usr/bin:/usr/sbin:/bin:/sbin",
     require => Package['python-pip'],
   }
 }
