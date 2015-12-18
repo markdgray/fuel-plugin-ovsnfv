23a653fb651c7843da92bf1da0d876c2e716d744
Billy O'Mahony <billy.o.mahony@intel.com>
2015-12-18 17:05:21 +0000

    network-ovs-dpdk: Merge many Ubuntu related changes
    The source for this is https://review.openstack.org/#/c/257486/6
    
    - due to package dependencies in puppet environment vanilla openvswitch package
    can't be simply removed but built binaries are replacing original files instead
    - dependent neutron services refreshed after ovs-dpdk is deployed
    - standard openstack bridges deployed on empty ovs db
    
    Signed-off-by: Billy O'Mahony <billy.o.mahony@intel.com>

--- deployment_scripts/puppet/manifests/ovs-install-ovsnfv.pp
+++ deployment_scripts/puppet/manifests/ovs-install-ovsnfv.pp
@@ -2,7 +2,7 @@ if $operatingsystem == 'Ubuntu' {
   class { '::ovsdpdk':
     ovs_bridge_mappings => '',
     ovs_socket_mem      => '512',
-    ovs_num_hugepages   => '300',
+    ovs_num_hugepages   => '256',
   }
 } elsif $operatingsystem == 'CentOS' {
 }
