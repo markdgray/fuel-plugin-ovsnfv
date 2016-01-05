d45c723fe18378c66ea6eeada1ebc51f4fec4987
Mark D. Gray <mark.d.gray@intel.com>
2016-01-05 12:52:57 +0000

    Change bridge mappings for compute node
    Signed-off-by: Mark D. Gray <mark.d.gray@intel.com>

--- deployment_scripts/puppet/manifests/ovs-install-ovsnfv-compute.pp
+++ deployment_scripts/puppet/manifests/ovs-install-ovsnfv-compute.pp
@@ -3,7 +3,7 @@ $master_ip = $::fuel_settings['master_ip']
 
 if $operatingsystem == 'Ubuntu' {
   class { '::ovsdpdk':
-    ovs_bridge_mappings => '',
+    ovs_bridge_mappings => 'default:br-eth3',
     ovs_socket_mem      => '512',
     ovs_num_hugepages   => '256',
     compute             => 'True',
